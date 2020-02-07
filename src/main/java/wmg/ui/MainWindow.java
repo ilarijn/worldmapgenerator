5cc24f3959a3075dcde3e44be325e80342a4a4d2
Ilari Junnonen <ilari.junnonen@iki.fi>
2020-02-07 22:32:31 +0200

    Update UI

--- src/main/java/wmg/ui/MainWindow.java
+++ src/main/java/wmg/ui/MainWindow.java
@@ -13,7 +13,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
-import javafx.scene.control.TextField;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
 import javafx.scene.layout.BorderPane;
